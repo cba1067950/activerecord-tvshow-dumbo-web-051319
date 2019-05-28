4c2121c8cec6ac683a52140a99e5efd8944d91da
Joe Jamsky <cba1067950@yahoo.com>
2019-05-28 13:00:02 +0000

    Automatically backed up by Learn

--- app/show.rb
+++ app/show.rb
@@ -1,28 +1,5 @@
 class Show < ActiveRecord::Base
   
-  def self.highest_rating 
-    self.maximum(:rating)
-  end 
-  
-  def self.most_popular_show
-    # self.highest_rating
-  end
-  
-  def self.lowest_rating 
-    self.minimum(:rating)
-  end 
-  
-  def self.least_popular_show 
-    
-  end 
-  
-  def self.popular_shows 
-    
-  end 
-  
-  def self.shows_by_alphabetical_order 
-    
-  end 
   
   
 end
